906e19cc408ea9ef21690176cd91af4e473fe507
RifalArdi25 <rifalardi25@gmail.com>
2025-03-17 15:49:29 +0700

    Menambahkan Program Perulangan Do WHILE

--- Contoh_Looping.cpp
+++ Contoh_Looping.cpp
@@ -45,5 +45,21 @@ int main()
     cout << "Jumlah Perulangan = " << perulanganWhile << endl;
     cout << "Nilai n terakhir = " << n << endl;
 
+    cout << "Perulangan do while" << endl;
+    
+    srand(time(0));
 
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    do
+    {
+            cout << "Nilai n = " << n << endl;
+            n = rand() % 10;
+            perulanganDo++;
+    } while (n < 7);
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+    
 }
