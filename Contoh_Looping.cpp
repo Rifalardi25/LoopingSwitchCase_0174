6c1209529f5439f1e58ecae9c2057d857ff115fc
RifalArdi25 <rifalardi25@gmail.com>
2025-03-17 15:19:53 +0700

    Menambahkan Using namespace dan deklarasi variabel

--- Contoh_Looping.cpp
+++ Contoh_Looping.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+using namespace std;
 
+int main()
+{// Deklarasi Variabel
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+
+}
